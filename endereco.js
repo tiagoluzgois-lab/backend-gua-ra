c5368d6584dc5166c311fc2209ed9c89f97f37e6
tiagoluzgois-lab <tiago.luz.gois@escola.pr.gov.br>
2026-03-10 14:39:10 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,10 @@
+console.log("Endereço Completo");
+
+let rua = "Rua das Flores";
+let numero = 489;
+let cidade = "Curitiba";
+
+let endereco = rua + ", " + numero + " - " + cidade;
+console.log(endereco);
+//console.log(endereço);
+console.log("O endereço correto é: " + endereco);
