3b19ed2b10ff8dd8fff4f9233a557c62fcbdb34a
tiagoluzgois-lab <tiago.luz.gois@escola.pr.gov.br>
2026-03-25 18:29:54 +0000

    versao1

---
+++ produto.js
@@ -0,0 +1,10 @@
+const produto = "Mochila Impermeável";
+let preco = 189.90;
+let estoque = 14;
+const categoria = "acessório";
+
+console.log(`O produto é: ${produto}.`);
+console.log(`A ${produto} custa R$ ${preco.toFixed(2)}.`);
+console.log(`O estoque ainda possui ${estoque} mochilas.`);
+console.log(`A categoria de ${produto} é ${categoria}.`);
+
