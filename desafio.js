4e740f8ba368f6fc7846d4cba3d2589dd8e589f1
tiagoluzgois-lab <tiago.luz.gois@escola.pr.gov.br>
2026-05-11 19:11:09 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,11 @@
+const produto = "Roupas";
+let preco = 380.00;
+let estoque = 25;
+const categoria = "acessório";
+const marca = "Archange";
+
+console.log(`O produto é: ${produto}.`);
+console.log(`A ${produto} custa R$ ${preco.toFixed(2)}.`);
+console.log(`O estoque ainda possui ${estoque} camisetas.`);
+console.log(`A categoria de ${produto} é ${categoria}.`);
+console.log(`Marca: ${marca}.`);
