6ae608affb5b1b35c9c25c9cebaafe489a111cbe
tiagoluzgois-lab <tiago.luz.gois@escola.pr.gov.br>
2026-05-18 19:25:24 +0000

    versao21

--- desafio.js
+++ desafio.js
@@ -9,3 +9,15 @@ console.log(`A ${produto} custa R$ ${preco.toFixed(2)}.`);
 console.log(`O estoque ainda possui ${estoque} camisetas.`);
 console.log(`A categoria de ${produto} é ${categoria}.`);
 console.log(`Marca: ${marca}.`);
+
+const numero1 = 20;
+const numero2 = 4;
+const resultadoSubtração = numero1 - numero2;
+const resultadoMultiplicação = numero1 * numero2;
+
+console.log(`O resultado da subtração é ${resultadoSubtração}, e o resultado da multiplicão é ${resultadoMultiplicação}.`)
+
+const Desconto = 12;
+const precoComDesconto = preco * (1 - Desconto / 100);
+
+console.log(`Preço com 12% de desconto: R$ ${precoComDesconto.toFixed(2)}.)
